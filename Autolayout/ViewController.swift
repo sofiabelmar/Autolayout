2d72a4800ee38cc882efa0264796942892298821
Alumno <ulsa@D113.local>
2020-10-26 15:34:32 -0700

    Vista landscape diferente

---
+++ Autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by Alumno on 10/26/20.
+//  Copyright © 2020 Estudiate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
